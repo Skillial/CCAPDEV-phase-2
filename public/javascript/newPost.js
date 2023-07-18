94369071371669538f71d2a514e948301961e7e9
Skillial <jeancabrera2003@gmail.com>
2023-07-18 10:52:57 +0800

    added new snacc can only be used by loggedin

--- public/javascript/newPost.js
+++ public/javascript/newPost.js
@@ -1,34 +1,36 @@
-const writePostButton = document.getElementById("newPostWrite");
-const postFormContainer = document.getElementById("newPostFormContainer");
-const postForm = document.getElementById("postForm");
-const cancelButton = document.getElementById("cancelButton");
-const yCancelButton = document.getElementById("yCancelButton");
-const nCancelButton = document.getElementById("nCancelButton");
+
 var maxCalls = 15;
 var callCount=0;
 
-writePostButton.addEventListener("click", () => {
-   postFormContainer.style.display = "block";
-});
+
+// const writePostButton = document.getElementById("newPostWrite");
+// const postFormContainer = document.getElementById("newPostFormContainer");
+// const postForm = document.getElementById("postForm");
+// const cancelButton = document.getElementById("cancelButton");
+// const yCancelButton = document.getElementById("yCancelButton");
+// const nCancelButton = document.getElementById("nCancelButton");
+// writePostButton.addEventListener("click", () => {
+//    postFormContainer.style.display = "block";
+// });
 
 
 
-cancelButton.addEventListener("click", () => { 
-  yCancelButton.style.display = "block";
-  nCancelButton.style.display = "block";
-});
+// cancelButton.addEventListener("click", () => { 
+//   yCancelButton.style.display = "block";
+//   nCancelButton.style.display = "block";
+// });
 
-yCancelButton.addEventListener("click", () => {
-    postForm.reset();
-    yCancelButton.style.display = "none";
-    nCancelButton.style.display = "none";
-    postFormContainer.style.display = "none";
-});
+// yCancelButton.addEventListener("click", () => {
+//     postForm.reset();
+//     yCancelButton.style.display = "none";
+//     nCancelButton.style.display = "none";
+//     postFormContainer.style.display = "none";
+// });
 
-nCancelButton.addEventListener("click", () => {
-  yCancelButton.style.display = "none";
-  nCancelButton.style.display = "none";
-});
+// nCancelButton.addEventListener("click", () => {
+//   yCancelButton.style.display = "none";
+//   nCancelButton.style.display = "none";
+// });
 
   
 function createPost(title, desc, author, rating, media, profile, link, pid, userReaction) {
